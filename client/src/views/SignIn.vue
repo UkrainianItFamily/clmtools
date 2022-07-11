3a4d0c750cd48eae12316867f0743b7c0b4a92f8
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-11 19:24:34 +0300

    position center for form

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="mt-4">
-            <div class="container">
-                <div class="box shadow-box">
+            <div class="d-flex justify-content-center">
+                <div class="box shadow-box w-50">
                     <h3 class="text-center mb-4">Authorization</h3>
                     <form
                         class="form"

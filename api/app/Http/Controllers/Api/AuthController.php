a0576c9ddbbd3b09bd8084c87d01782582538b36
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-13 21:26:11 +0300

    add reset method for auth controller

--- api/app/Http/Controllers/Api/AuthController.php
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -9,11 +9,16 @@ use App\Actions\Auth\ForgotPasswordRequest;
 use App\Actions\Auth\ForgotPasswordResponse;
 use App\Actions\Auth\LoginAction;
 use App\Actions\Auth\LoginRequest;
+use App\Actions\Auth\ResetPasswordAction;
+use App\Actions\Auth\ResetPasswordRequest;
+use App\Actions\Auth\ResetPasswordResponse;
 use App\Http\Presenters\AuthenticationResponseArrayPresenter;
 use App\Http\Requests\Api\Auth\AuthRequest;
 use App\Http\Requests\Api\Auth\PasswordResetLinkRequest;
+use App\Http\Requests\Api\Auth\ResetRequest;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 final class AuthController extends ApiController
 {
@@ -51,6 +56,25 @@ final class AuthController extends ApiController
         return $response == Password::RESET_LINK_SENT
             ? $this->successResponse($forgotPasswordResponse->sendResetLinkResponse($request, $response))
             : $this->errorResponse($forgotPasswordResponse->sendResetLinkFailedResponse($request, $response));
+    }
+
+    public function reset(
+        ResetRequest $resetRequest,
+        ResetPasswordAction $action,
+        ResetPasswordResponse $resetPasswordResponse
+    ) {
+        $request = new ResetPasswordRequest(
+            $resetRequest->token,
+            $resetRequest->email,
+            $resetRequest->password,
+            $resetRequest->password_confirmation
+        );
+
+        $response = $action->execute($request);
+
+        return $response == Password::PASSWORD_RESET
+            ? $this->successResponse($resetPasswordResponse->sendResetResponse($request, $response))
+            : $this->errorResponse($resetPasswordResponse->sendResetFailedResponse($request, $response));
     }
 
 }

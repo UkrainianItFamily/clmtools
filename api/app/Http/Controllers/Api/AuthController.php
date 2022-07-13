aa505beecaf2fc460b0601b27d11a4fbfef0bb65
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-13 20:30:30 +0300

    add method for forgot password

--- api/app/Http/Controllers/Api/AuthController.php
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Auth\ForgotPasswordAction;
+use App\Actions\Auth\ForgotPasswordRequest;
+use App\Actions\Auth\ForgotPasswordResponse;
 use App\Actions\Auth\LoginAction;
 use App\Actions\Auth\LoginRequest;
 use App\Http\Presenters\AuthenticationResponseArrayPresenter;
 use App\Http\Requests\Api\Auth\AuthRequest;
+use App\Http\Requests\Api\Auth\PasswordResetLinkRequest;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 final class AuthController extends ApiController
 {
+
     public function login(
         AuthRequest $authRequest,
         LoginAction $action,
@@ -29,4 +36,21 @@ final class AuthController extends ApiController
 
         return $this->successResponse($authenticationResponseArrayPresenter->present($response));
     }
+
+    public function forgotPassword(
+        PasswordResetLinkRequest $passwordResetLinkRequest,
+        ForgotPasswordAction $action,
+        ForgotPasswordResponse $forgotPasswordResponse
+    ) {
+        $request = new ForgotPasswordRequest(
+            $passwordResetLinkRequest->email
+        );
+
+        $response = $action->execute($request);
+
+        return $response == Password::RESET_LINK_SENT
+            ? $this->successResponse($forgotPasswordResponse->sendResetLinkResponse($request, $response))
+            : $this->errorResponse($forgotPasswordResponse->sendResetLinkFailedResponse($request, $response));
+    }
+
 }

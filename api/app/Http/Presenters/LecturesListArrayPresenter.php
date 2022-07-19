363c40b962ffa433d337b8b6d33974feb5d9ce7c
Denis Velykyi <denis.velykyi@ukrainian-it.family>
2022-07-19 12:00:20 +0300

    testing and corrections

--- api/app/Http/Presenters/LecturesListArrayPresenter.php
+++ api/app/Http/Presenters/LecturesListArrayPresenter.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Presenters;
 
-use App\Actions\Lecture\LectureResponse;
+use App\Actions\Lecture\LectureArrayResponse;
 
 final class LecturesListArrayPresenter
 {
-    public function present(LectureResponse $lecture): array
+    public function present(LectureArrayResponse $lecture): array
     {
         return [
             'lectures' => $lecture->getLecture()

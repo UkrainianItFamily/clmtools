f0fe325422289eaeae7c820f7e20f8d2cdcd1639
D.Yaguza <dmitry.yaguza@ukrainian-it.family>
2022-07-19 15:49:26 +0300

    Add functionality to LeftMenu component

--- client/src/components/common/LeftMenu.vue
+++ client/src/components/common/LeftMenu.vue
@@ -1,19 +1,34 @@
 <template>
-    <div class="col-lg-3">
-        <h4 class="mt-5 font-weight-bold text-center">Меню</h4>
-        <ul class="nav nav-link-secondary flex-column">
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="#"><span>Лекції</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="#"><span>Завдання</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="#"><span>База додаткової інформації</span></a>
-            </li>
-        </ul>
-    </div>
+  <div class="col-lg-3">
+    <h4 class="mt-5 font-weight-bold text-center">Меню</h4>
+    <ul class="nav nav-link-secondary flex-column">
+      <li class="nav-item" v-bind:key="link.title" v-for="link in links">
+        <RouterLink class="nav-link text-dark" :to="{ name: link.route }">{{ link.title }}</RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        links: [
+          {
+            title: "Завдання",
+            route: "Tasks"
+          },
+          {
+            title: "База додаткової інформації",
+            route: "MoreInfo"
+          },
+        ]
+      };
+    }
+  };
 </script>
+<style lang="scss" scoped>
+    .router-link-active {
+        font-weight: 700;
+    }
+</style>
--- client/src/views/MoreInfo.vue
+++ client/src/views/MoreInfo.vue
@@ -1,23 +1,5 @@
 <template>
-    <section class="row g-4">
-        <div class="col-lg-3">
-            <h4 class="mt-5 font-weight-bold text-center">Меню</h4>
-            <ul class="nav nav-link-secondary flex-column">
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="my-profile.html"><span>Лекції</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="my-profile-connections.html"><span>Завдання</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="blog.html"><span>База додаткової інформації</span></a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-lg-9">
-            <h1 class="mt-5 font-weight-bold text-primary">MoreInfo page</h1>
-        </div>
-    </section>
+    <h1 class="mt-5 font-weight-bold text-primary">MoreInfo page</h1>
 </template>
 
 <script>

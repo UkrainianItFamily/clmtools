90b8fc7bf312ca689bf9b84a1c5e8eb6c77508c3
D.Yaguza <dmitry.yaguza@ukrainian-it.family>
2022-07-26 16:14:51 +0300

    Add MessageArrayPresenter with related functions

---
+++ api/app/Http/Presenters/MessageArrayPresenter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Presenters;
+
+use App\Models\Message;
+use Illuminate\Support\Collection;
+
+final class MessageArrayPresenter
+{
+    private UserArrayPresenter $userArrayPresenter;
+    private ChatArrayPresenter $chatArrayPresenter;
+
+    public function __construct()
+    {
+        $this->userArrayPresenter = new UserArrayPresenter();
+        $this->chatArrayPresenter = new ChatArrayPresenter();
+    }
+
+    public function present(Message $message): array
+    {
+        return [
+            'id' => $message->getId(),
+            'user' => $this->userArrayPresenter->present($message->getUser()),
+            'chat' => $this->chatArrayPresenter->present($message->getChat()),
+            'body' => $message->getBody(),
+            'created_at' => $message->getCreatedAt()->toDateTimeString()
+        ];
+    }
+
+    public function presentCollection(Collection $collection): array
+    {
+        return $collection
+            ->map(
+                function (Message $message) {
+                    return $this->present($message);
+                }
+            )
+            ->all();
+    }
+}
--- api/app/Models/Message.php
+++ api/app/Models/Message.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Message extends Model
 {
@@ -22,5 +23,30 @@ class Message extends Model
     public function getSelfMessageAttribute()
     {
         return $this->user_id === auth()->user()->id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getChat(): Chat
+    {
+        return $this->chat;
+    }
+
+    public function getCreatedAt(): Carbon
+    {
+        return $this->created_at;
     }
 }

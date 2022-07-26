d375a7d4f93ee23cae28ada596aaecd71c1df4c6
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-26 19:13:54 +0300

    add method get collection in city presenter

--- api/app/Http/Presenters/CityArrayPresenter.php
+++ api/app/Http/Presenters/CityArrayPresenter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Presenters;
 
+use App\Actions\Handbook\CityCollectionResponse;
+use App\Actions\Handbook\CityResponse;
 use App\Models\City;
 
 final class CityArrayPresenter
@@ -14,5 +16,16 @@ final class CityArrayPresenter
             'id' => $city->getId(),
             'name' => $city->getName(),
         ];
+    }
+
+    public function getCollections(CityCollectionResponse $cities): array
+    {
+        return $cities->getCity()
+            ->map(
+                function (City $city) {
+                    return $this->present(new CityResponse($city));
+                }
+            )
+            ->all();
     }
 }

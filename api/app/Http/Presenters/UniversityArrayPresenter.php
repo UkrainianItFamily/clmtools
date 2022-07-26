03ad731bfee0fe539b7ea239aa4b8c6683920b18
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-26 19:32:13 +0300

    add get collection method in university presenter

--- api/app/Http/Presenters/UniversityArrayPresenter.php
+++ api/app/Http/Presenters/UniversityArrayPresenter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Presenters;
 
+use App\Actions\Handbook\UniversityCollectionResponse;
+use App\Actions\Handbook\UniversityResponse;
 use App\Models\University;
 
 final class UniversityArrayPresenter
@@ -14,5 +16,17 @@ final class UniversityArrayPresenter
             'id' => $university->getId(),
             'name' => $university->getName(),
         ];
+    }
+
+
+    public function getCollections(UniversityCollectionResponse $cities): array
+    {
+        return $cities->getUniversity()
+            ->map(
+                function (University $university) {
+                    return $this->present(new UniversityResponse($university));
+                }
+            )
+            ->all();
     }
 }

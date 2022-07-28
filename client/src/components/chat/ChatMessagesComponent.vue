c1d8d02151f0ad9f5d1438f7a0e164185e0d0bd0
D.Yaguza <dmitry.yaguza@ukrainian-it.family>
2022-07-28 10:35:24 +0300

    Add display chat messages functionality

--- client/src/components/chat/ChatMessagesComponent.vue
+++ client/src/components/chat/ChatMessagesComponent.vue
@@ -1,16 +1,47 @@
 <template>
     <div class="message-area">
-        <MessageComponent></MessageComponent>
+        <MessageComponent
+            v-for="message in getChat"
+            :key="message.id"
+            :message="message"
+        ></MessageComponent>
     </div>
 </template>
 
 <script>
 import MessageComponent from '@/components/chat/MessageComponent.vue';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: 'ChatMessagesComponent',
     components:{
         MessageComponent
     },
+    computed: {
+        ...mapGetters('auth', {
+            user: 'getAuthenticatedUser'
+        }),
+        ...mapGetters('chat', [
+           'getChat',
+        ]),
+    },
+    methods: {
+        ...mapActions('chat', [
+            'GET_CHAT',
+        ]),
+    },
+    mounted() {
+        this.GET_CHAT({
+            user_id: this.user.id,
+            lecture_id: this.$route.params.lection_id
+        })
+            .then(() => {})
+            .catch((error) => {
+                if (error.response.data.errors) {
+                    alert(Object.values(error.response.data.errors).join('\r\n'));
+                }
+            } );
+    },
+
 };
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import StatusService from './modules/status-service';
 import auth from './modules/auth';
 import lections from './modules/lections';
+import chat from './modules/chat';
 
 Vue.use(Vuex);
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
   modules: {
     StatusService,
       auth,
-      lections
+      lections,
+      chat
   }
 });

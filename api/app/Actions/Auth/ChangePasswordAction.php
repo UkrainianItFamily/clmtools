79bc36108d62630210d79a615df249b4b369f6b6
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-28 10:35:08 +0300

    add change password action

---
+++ api/app/Actions/Auth/ChangePasswordAction.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Auth;
+
+use App\Exceptions\InvalidChangePasswordNewPasswordEqualOldPasswordException;
+use App\Exceptions\InvalidChangePasswordWrongOldPasswordException;
+use App\Models\User;
+use App\Repository\UserRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+final class ChangePasswordAction
+{
+    public function execute(ChangePasswordRequest $request): void
+    {
+        $user = Auth::user();
+
+        if(Hash::check($request->getOldPassword(),$user->password) == false)
+        {
+            throw new InvalidChangePasswordWrongOldPasswordException();
+        }
+        else if (Hash::check($request->getNewPassword(),$user->password))
+        {
+            throw new InvalidChangePasswordNewPasswordEqualOldPasswordException();
+        }
+        else{
+            $user->forceFill(['password' => Hash::make($request->getNewPassword())])->save();
+        }
+    }
+}

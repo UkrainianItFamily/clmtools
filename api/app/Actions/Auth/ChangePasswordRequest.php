88eb880ce41959fab1542ff9fc1c67a5505b0476
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-28 10:48:15 +0300

    update change password request

--- api/app/Actions/Auth/ChangePasswordRequest.php
+++ api/app/Actions/Auth/ChangePasswordRequest.php
@@ -7,18 +7,24 @@ namespace App\Actions\Auth;
 final class ChangePasswordRequest
 {
     public function __construct(
-        private string $oldPassword,
-        private string $newPassword
+        private string $old_password,
+        private string $new_password,
+        private string $new_password_confirmation
     ) {
     }
 
     public function getOldPassword(): string
     {
-        return $this->oldPassword;
+        return $this->old_password;
     }
 
     public function getNewPassword(): string
     {
-        return $this->newPassword;
+        return $this->new_password;
+    }
+
+    public function getNewPasswordConfirmation(): string
+    {
+        return $this->new_password_confirmation;
     }
 }

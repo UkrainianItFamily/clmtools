d469d99d9f018ccb3e6a91bdb44e56df81b6a4be
D.Yaguza <dmitry.yaguza@ukrainian-it.family>
2022-07-29 14:29:20 +0300

    Add getChat function usage

--- client/src/components/chat/ChatComponent.vue
+++ client/src/components/chat/ChatComponent.vue
@@ -4,9 +4,12 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Чат з викладачем</div>
-                <div class="card-body">
+                <div class="card-body" v-if="chatExist">
                     <ChatMessagesComponent></ChatMessagesComponent>
                     <ChatFormComponent></ChatFormComponent>
+                </div>
+                <div class="card-body" v-else>
+                    <button>Почати листування</button>
                 </div>
             </div>
         </div>
@@ -17,6 +20,7 @@
 import ChatMessagesComponent from '@/components/chat/ChatMessagesComponent.vue';
 import ChatFormComponent from '@/components/chat/ChatFormComponent.vue';
 import StudentLectionMenu from '@/components/common/StudentLectionMenu.vue';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
     name: 'ChatComponent',
@@ -24,6 +28,39 @@ export default {
         ChatMessagesComponent,
         ChatFormComponent,
         StudentLectionMenu
+    },
+    data: () => ({
+        chatExist: false,
+    }),
+    computed: {
+        ...mapGetters('auth', {
+            user: 'getAuthenticatedUser'
+        }),
+        ...mapGetters('chat', [
+            'getChat',
+        ]),
+    },
+    methods: {
+        ...mapActions('chat', [
+            'GET_CHAT',
+        ]),
+    },
+    mounted() {
+        this.GET_CHAT({
+            user_id: this.user.id,
+            lecture_id: this.$route.params.lection_id
+        })
+            .then(() => {
+                console.log(this.getChat.id);
+                if(this.getChat.id){
+                    this.chatExist = true;
+                }
+            })
+            .catch((error) => {
+                if (error.response.data.errors) {
+                    alert(Object.values(error.response.data.errors).join('\r\n'));
+                }
+            } );
     },
 };
 </script>

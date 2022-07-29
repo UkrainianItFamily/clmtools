c17c645445f5b59f435326b1208425010337ad7a
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-29 15:39:47 +0300

    add pad method

--- client/src/components/methods/methods.js
+++ client/src/components/methods/methods.js
@@ -1,15 +1,22 @@
 import moment from 'moment';
-export default{
-    methods:{
-        getDaysInMonthArray(year, month){
+
+export default {
+    methods: {
+        getDaysInMonthArray(year, month)
+        {
             let day_arr = [];
             let maxDays = moment(year + '-' + month, 'YYYY-MM').daysInMonth();
 
-            for (let i = 1; i <= maxDays; i++) {
-                (i < 10) ? day_arr.push('0'+i) : day_arr.push(i.toString());
+            for (let i = 1; i <= maxDays; i++)
+            {
+                (i < 10) ? day_arr.push('0' + i) : day_arr.push(i.toString());
             }
             return day_arr;
         },
+        pad(d)
+        {
+            return (d < 10) ? '0' + d.toString() : d.toString();
+        },
 
-    }
+    },
 };
